b2fd2cddde64d95ca443e5077967a262540b4e53
TJ-Rocke <spook.devv@gmail.com>
2025-05-14 20:41:21 -0400

    Working with classes (public and private key words)

--- basics.js
+++ basics.js
@@ -1,25 +1,26 @@
+"use strict";
 // class User {
 //   name: string;
 //   age: number;
-
 //   constructor(name: string, age: number) {
 //     this.name = name;
 //     this.age = age;
 //   }
 // }
-
 // a shortcut to making classes without fields, add 'public' keyword in front of constructor parameters. Using 'public' keyword will automatically create fields with the parameter name to the class
 class User {
-  // public is default so the 'public' keyword isnt necessary (can make code easier to read and understand tho)
-  public hobbies: string[] = []; // public/private marked properties affects how properties are accessed in/outside of the class
-
-  constructor(public name: string, private age: number) {}
-
-  greet() {
-    console.log("My age: " + this.age);
-  }
+    name;
+    age;
+    // public is default so the 'public' keyword isnt necessary (can make code easier to read and understand tho)
+    hobbies = []; // public/private marked properties affects how properties are accessed in/outside of the class
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log("My age: " + this.age);
+    }
 }
-
 const anthony = new User("Anthony", 23);
 console.log(anthony.hobbies);
 // console.log(anthony.age) // uncomment and read error message (cannot access a private property called 'age')
