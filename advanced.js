699554e55434cc1983ba66f50e12e6e1ae622eb4
TJ-Rocke <spook.devv@gmail.com>
2025-05-14 21:04:47 -0400

    Static properties & methods

--- advanced.js
+++ advanced.js
@@ -19,7 +19,9 @@ class User {
         // because 'get' keyword exists, it automatically will access and return the function as a property (e.g. tony.fullName = )
         return this._firstName + " " + this._lastName;
     }
+    static eid = "USER"; // static property can be accesses on the class itself
 }
+console.log(User.eid);
 const tony = new User();
 tony.firstName = "Tony"; // firstName and lastName can be accessed and set because the setter isn't set to private
 tony.lastName = "Gambino";
--- advanced.ts
+++ advanced.ts
@@ -21,7 +21,16 @@ class User {
     // because 'get' keyword exists, it automatically will access and return the function as a property (e.g. tony.fullName = )
     return this._firstName + " " + this._lastName;
   }
+
+  static eid = "USER"; // static property can be accesses on the class itself (e.g. User.eid, e.g. User.greet)
+
+  static greet() {
+    console.log("Hello World");
+  }
 }
+
+console.log(User.eid);
+User.greet();
 
 const tony = new User();
 tony.firstName = "Tony"; // firstName and lastName can be accessed and set because the setter isn't set to private
